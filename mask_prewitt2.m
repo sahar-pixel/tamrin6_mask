e15c41b9755728384647b5df3817e955b9255526
sahar-pixel <148361671+sahar-pixel@users.noreply.github.com>
2023-12-05 01:22:06 +0330

    tamrin6
    666

---
+++ mask_prewitt2.m
@@ -0,0 +1,16 @@
+clc; clear; close all;   %prewitt :Gy
+img = rgb2gray(imread('photo.jpg'));
+mask1 = [-1 ,-1,-1;0,0,0;1,1,1];
+img2 = zeros(size(img),'uint8');
+for i=2:size(img,1)-1
+    for j=2:size(img,2)-1
+        sum = 0;
+        for x=1:3
+            for y=1:3
+                sum = sum + (img(i+x-2,j+y-2)*mask1(x,y));
+            end
+        end
+        img2(i,j) = sum;
+    end
+end
+imshow(img);figure;imshow(img2);
